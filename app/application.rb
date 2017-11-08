b6dfdc711cadecb5c5a1ddadaad9fd1b30baf5f2
Justin Tooley <jstooley823@gmail.com>
2017-11-08 00:05:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,10 @@ class Application
    def call(env)
      resp = Rack::Response.new
      req = Rack::Request.new(env)
- 
+
      if req.path.match(/items/)
 
-       item_name = req.path.split("/items/").last
+       item_name = req.path.split("/").last
        item = @@items.find{|i| i.name == item_name}
        binding.pry
 
